1541c5dff462f4c493de695f1cc4f594d43b9616
vohaohuy1207 <vohaohuy1998vn@gmail.com>
2023-11-09 15:07:24 +0700

    add swiper and hot promotion section

---
+++ samsung-landing/js/functions.js
@@ -0,0 +1,25 @@
+
+// core version + navigation, pagination modules:
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });

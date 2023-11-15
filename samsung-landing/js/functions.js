721f43863bdde4b11c6c3c435d99d24a153e15bc
vohaohuy1207 <vohaohuy1998vn@gmail.com>
2023-11-15 17:30:27 +0700

    add phone list section and tivi landing

--- samsung-landing/js/functions.js
+++ samsung-landing/js/functions.js
@@ -15,3 +15,64 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+$(document).ready(function () {
+  $(document).on("click", function (e) {
+    const $openDropdown = $('.js-dropdown-menu')
+    if ($(e.target).is(".js-dropdown-button") === false) {
+      if ($openDropdown.hasClass('open')) {
+        $openDropdown.removeClass('open');
+      }
+    }
+  });
+
+  // dropdown
+  function openDropdown() {
+
+    const dropdown = $('.js-dropdown');
+
+    dropdown.each(function (index, item) {
+      const dropdownBtn = $(this).find('.js-dropdown-button'),
+        dropdownMenu = $(this).find('.js-dropdown-menu'),
+        dropdownItem = $(this).find('.js-dropdown-item');
+
+      dropdownBtn.click(function () {
+        dropdownMenu.removeClass('open');
+        dropdownMenu.addClass('open');
+      })
+
+      dropdownItem.click(function (event) {
+
+        const thisVal = $(this).text();
+
+        event.preventDefault();
+
+        dropdownBtn.text(thisVal);
+
+        dropdownItem.removeClass('active');
+        $(this).addClass('active');
+      })
+    })
+  };
+
+  //click product memory buttons
+
+  function clickProductMemory() {
+    const memoryContainers = $('.js-product-memory-list');
+
+    memoryContainers.each(function (index, item) {
+      const btnMemory = $(this).find('.js-product-memory-item');
+
+      btnMemory.click(function () {
+        btnMemory.removeClass('active');
+        $(this).addClass('active');
+      })
+    })
+  }
+
+  const init = function () {
+    openDropdown();
+    clickProductMemory();
+  }();
+
+});
